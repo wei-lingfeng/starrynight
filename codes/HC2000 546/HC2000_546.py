62e95d46bd46ec6cb5326c090f7fa4ad0c3580f8
Lingfeng Wei <l3wei@ucsd.edu>
2023-05-18 17:50:05 -0700

    Update m grids and save new results

--- codes/HC2000 546/HC2000_546.py
+++ codes/HC2000 546/HC2000_546.py
@@ -32,10 +32,15 @@ dates_observe = [
 
 t = [_.days for _ in np.diff(dates_observe)]
 t.insert(0, 0)
+delta_t = t[-1] * u.day
+
+# rv = [29.14, 21.337643, 25.994355] * u.km/u.s
+# rv_err = [0.38, 0.27737549, 0.19132987] * u.km/u.s
+# t = [0, 330, 638] * u.day
 
 data = RVData(t=t, rv=rv, rv_err=rv_err)
 P_min = 10*u.day
-P_max = 600*u.day
+P_max = 2*t[-1]*u.day
 
 if new_sampling:
     prior = JokerPrior.default(
@@ -45,12 +50,9 @@ if new_sampling:
         sigma_v=100*u.km/u.s
     )
     joker = TheJoker(prior)
-
     prior_samples = prior.sample(size=100000)
     samples = joker.rejection_sample(data, prior_samples)
-
     samples.write(f'{user_path}/ONC/starrynight/codes/HC2000 546/samples.hdf5', overwrite=True)
-    
 else:
     samples = tj.JokerSamples.read(f'{user_path}/ONC/starrynight/codes/HC2000 546/samples.hdf5')
 
@@ -79,36 +81,43 @@ e = e[valid_idx]
 m = np.array(m)
 a = np.array(a)
 
-# plot sma-mass
-lower_bound = lambda m : ((M + m) * P_min.to(u.yr).value**2)**(1/3) # a > a_P_min
-upper_bound = lambda m : ((M + m) * P_max.to(u.yr).value**2)**(1/3) # a < a_P_max
-left_bound_circular = lambda m : 4*np.pi**2*m**2/(M + m) / (rv.ptp().to(u.au/u.yr).value/2)**2 # a < a_dv_min
-left_bound = lambda m, e : left_bound_circular(m) / (1 - e**2)
+period = lambda m, P : ((M + m) * P.to(u.yr).value**2)**(1/3)   # define sma(mass, period), period constraint derived from Kepler's third law
+lower_bound = lambda m : period(m, P_min) # a > sma(mass, P_min)
+upper_bound = lambda m : period(m, P_max) # a < sma(mass, P_max)
+left_bound_circular = lambda m : 4*np.pi**2*m**2/(M + m) / (rv.ptp().to(u.au/u.yr).value/2)**2 # a < sma(mass, e=0), Δv constraint derived from K (see Murray & Correia 2010 equation 66).
+left_bound = lambda m, e : left_bound_circular(m) / (1 - e**2)  # a < sma(mass, e)
 
 print(f'Maximum e for a < left_bound_circular: {max(e[a > left_bound_circular(m)]):.2f}')
 e_max = 0.9
 
-m_intersection_lower = fsolve(lambda m: left_bound(m, e_max) - lower_bound(m), x0=0.01)[0]
-m_intersection_upper = fsolve(lambda m: left_bound(m, e_max) - upper_bound(m), x0=0.05)[0]
-m_intersection_lower_circular = fsolve(lambda m: left_bound_circular(m) - lower_bound(m), x0=min(m))[0]
-m_intersection_upper_circular = fsolve(lambda m: left_bound_circular(m) - upper_bound(m), x0=0.12)[0]
+Ps = [P_min, delta_t/4, delta_t/3, delta_t/2, delta_t, P_max]
+solve_intersection = lambda m, P, e : left_bound(m, e) - period(m, P) # solve for m intersections where Δv constraint = period constraint
+m_intersections = np.array([fsolve(lambda m : solve_intersection(m, _, e_max), x0=0.03)[0] for _ in Ps]) # intersection between period and e_max
+m_intersections_circular = np.array([fsolve(lambda m : solve_intersection(m, _, 0), x0=0.03)[0] for _ in [P_min, P_max]]) # intersection between period and e=0
 
-m_grid_left = np.linspace(m_intersection_lower, m_intersection_upper, 100)
-m_grid_right = np.linspace(m_intersection_upper, M, 100)
-m_grid = np.concatenate((m_grid_left, m_grid_right)).flatten()
-m_grid_left_circular = np.linspace(m_intersection_lower_circular, m_intersection_upper_circular, 100)
+# m_grid for boundaries
+m_grids = [np.linspace(m_intersection, M, 100) for m_intersection in m_intersections]
+m_grid_left = np.linspace(m_intersections[0], m_intersections[-1], 100) # m grid for left bound
+m_grid_left_circular = np.linspace(m_intersections_circular[0], m_intersections_circular[-1], 100)  # m grid for Δv constraint with e=0
+m_grid_fill = np.concatenate((m_grid_left, m_grids[-1])).flatten()   # m grid for fill
 
-fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 5))
 ax.scatter(m, a, 1, color='C7', marker='.', label='Sampled Systems')
-ax.plot(m_grid, lower_bound(m_grid), lw=2, label=f'Min. Period: {P_min.value:.0f} days', zorder=5)
-ax.plot(m_grid_right, upper_bound(m_grid_right), lw=2, linestyle='-.', label=f'Max. Period: {P_max.value:.0f} days', zorder=4)
-ax.plot(m_grid_left_circular, left_bound_circular(m_grid_left_circular), lw=2, linestyle='--', label=r'Min. $\Delta v$ with $e = 0$', zorder=3)
-ax.plot(m_grid_left, left_bound(m_grid_left, e_max), lw=2, linestyle='--', label=fr'Min. $\Delta v$ with $e = {e_max:.1f}$', zorder=2)
-ax.vlines(M, lower_bound(M), upper_bound(M), lw=2, color='k', linestyle=':', label='Max. Companion Mass', zorder=1)
+ax.plot(m_grids[0], lower_bound(m_grids[0]), linewidth=2, label=f'Min. Period: {P_min.value:.0f} days', zorder=5)
+ax.plot(m_grids[-1], upper_bound(m_grids[-1]), linewidth=2, linestyle='-.', label=f'Max. Period: {P_max.value:.0f} days', zorder=4)
+ax.plot(m_grid_left_circular, left_bound_circular(m_grid_left_circular), linewidth=2, linestyle='--', label=r'Min. $\Delta v$ with $e = 0$', zorder=3)
+ax.plot(m_grid_left, left_bound(m_grid_left, e_max), linewidth=2, linestyle='--', label=rf'Min. $\Delta v$ with $e = {e_max:.1f}$', zorder=2)
+ax.vlines(M, lower_bound(M), upper_bound(M), linewidth=2, color='k', label='Max. Companion Mass', zorder=1)
+for i in range(1, len(m_grids)-1):
+    if i==1:
+        ax.plot(m_grids[i], period(m_grids[i], Ps[i]), color='C4', linewidth=1.5, linestyle=':', zorder=0, label='Period Gaps')
+    else:
+        ax.plot(m_grids[i], period(m_grids[i], Ps[i]), color='C4', linewidth=1.5, linestyle=':', zorder=0)
 ax.fill_between(
-    m_grid, 
-    lower_bound(m_grid),
-    np.concatenate((left_bound(m_grid_left, e_max), upper_bound(m_grid_right))).flatten(),
+    m_grid_fill, 
+    lower_bound(m_grid_fill),
+    np.concatenate((left_bound(m_grid_left, e_max), upper_bound(m_grids[-1]))).flatten(),
     color='C7',
     alpha=0.2,
     label='Allowed Companion',
@@ -116,31 +125,39 @@ ax.fill_between(
 )
 ax.set_xscale('log')
 ax.set_xlim(right=0.78)
-ax.set_ylim(bottom=-0.1)
-ax.annotate(fr'$P={P_min.value:.0f}$ days', xy=(0.1, -0.02), horizontalalignment='center', verticalalignment='center', size=11)
-ax.annotate(fr'$P={P_max.value:.0f}$ days', xy=(0.15, 1.3), horizontalalignment='center', verticalalignment='center', size=11, rotation=9)
-ax.annotate(fr'$\Delta v_\mathrm{{min}}$, $e={e_max:.1f}$', xy=(0.027, 0.6), horizontalalignment='center', verticalalignment='center', size=11, rotation=60)
-ax.annotate(fr'$\Delta v_\mathrm{{min}}$, $e=0$', xy=(0.065, 0.6), horizontalalignment='center', verticalalignment='center', size=11, rotation=60)
-ax.annotate(fr'$m_\mathrm{{max}}={M:.2f}~M_\odot$', xy=(0.6, 0.7), horizontalalignment='center', verticalalignment='center', size=11, rotation=-90)
+ax.set_ylim(bottom=-0.2)
+
+log_percentile = lambda x, percentile : 10**(np.percentile(np.log10(np.array(x)[[0, -1]]), percentile))
+log_middle = lambda x : log_percentile(x, 50)
+ax.annotate(rf'$P={P_min.value:.0f}$ days', xy=(log_percentile(m_grids[0], 60), lower_bound(log_percentile(m_grids[0], 60)) - 0.05), horizontalalignment='center', verticalalignment='top', size=11)
+ax.annotate(rf'$P={P_max.value:.0f}$ days', xy=(log_middle(m_grids[-1]) - 0.02, upper_bound(log_middle(m_grids[-1])) - 0.05), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=12)
+ax.annotate(rf'$\Delta v_\mathrm{{min}}$, $e={e_max:.1f}$', xy=(log_percentile(m_grid_left, 70), left_bound(log_percentile(m_grid_left, 70), e_max)), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=68)
+ax.annotate(rf'$\Delta v_\mathrm{{min}}$, $e=0$', xy=(log_percentile(m_grid_left_circular, 70), left_bound_circular(log_percentile(m_grid_left_circular, 70))), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=67)
+ax.annotate(rf'$m_\mathrm{{max}}={M:.2f}~M_\odot$', xy=(0.6, (lower_bound(M) + upper_bound(M))/2), horizontalalignment='center', verticalalignment='center', size=11, rotation=-90)
+
+ax.annotate('$P=\Delta t$', xy=(0.3, period(0.3, Ps[-2])), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=12)
+ax.annotate('$P=\Delta t/2$', xy=(0.3, period(0.3, Ps[-3])), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=8)
+ax.annotate('$P=\Delta t/3$', xy=(0.3, period(0.3, Ps[-4]) - 0.01), horizontalalignment='center', verticalalignment='bottom', size=11, rotation=7)
+ax.annotate('$P=\Delta t/4$', xy=(0.3, period(0.3, Ps[-5]) - 0.01), horizontalalignment='center', verticalalignment='top', size=11, rotation=6)
+
 ax.xaxis.set_major_formatter(mticker.ScalarFormatter()) # set to regular format
-ax.set_xticks([0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.5])
-ax.set_xlabel(r'Companion Mass $\left(M_\odot\right)$')
-ax.set_ylabel('Semi-Major Axis (au)')
+ax.set_xticks([0.02, 0.03, 0.05, 0.07, 0.1, 0.2, 0.3, 0.5])
+ax.set_xlabel(r'Companion Mass $\left(M_\odot\right)$', fontsize=12)
+ax.set_ylabel('Semi-Major Axis (au)', fontsize=12)
 handles, labels = ax.get_legend_handles_labels()
 handles[0] = Line2D([], [], marker='.', color='C7', label='Sampled Systems', markersize=5, linestyle='None')
-ax.legend(handles=handles, loc='lower left', bbox_to_anchor=(1, -0.023))
+ax.legend(handles=handles, loc='lower left', bbox_to_anchor=(1, -0.023), fontsize=12)
 plt.savefig(f'{user_path}/ONC/figures/HC2000 546 - Allowed Param.pdf', bbox_inches='tight')
 plt.show()
 
 # only plot orbits with period > 300 days
-plot_orbit = P > (300/365.25)
+plot_orbit = P > (max(t)/3/365.25)
 print(f'{sum(plot_orbit)} orbits plotted.')
-samples = samples[plot_orbit]
 
 fig, ax = plt.subplots(1, 1, figsize=(6, 4)) # doctest: +SKIP
 t_grid = np.linspace(-60, t[-1]+60, 1024)
-plot_rv_curves(samples, t_grid, rv_unit=u.km/u.s, data=data, ax=ax,
-               plot_kwargs=dict(color='C7'), data_plot_kwargs=dict(color='k', marker='.', markersize=5, elinewidth=1.2, capsize=3, capthick=1.2))
+plot_rv_curves(samples[plot_orbit], t_grid, rv_unit=u.km/u.s, data=data, ax=ax,
+               plot_kwargs=dict(color='C7', linewidth=0.4), data_plot_kwargs=dict(color='k', marker='.', markersize=5, elinewidth=1.2, capsize=3, capthick=1.2))
 ax.errorbar(t[0], rv[0], yerr=rv_err[0], color='C0', fmt='.', markersize=5, elinewidth=1.2, capsize=3, capthick=1.2, label='APOGEE')
 ax.errorbar(t[1:], rv[1:], yerr=rv_err[1:], color='C3', fmt='.', markersize=5, elinewidth=1.2, capsize=3, capthick=1.2, label='NIRSPAO')
 ax.set_ylim((min(rv.value) - 5, max(rv.value) + 7))
